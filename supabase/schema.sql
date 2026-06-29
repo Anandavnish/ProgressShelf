b9f1527b244a46d5f860926379c61ec91f453207
Anandavnish <anandavnish1753@gmail.com>
2026-06-29 09:47:01 +0530

    Enable pg_net extension in schema.sql to fix cron HTTP posts

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,5 +1,6 @@
--- Enable pg_cron
+-- Enable pg_cron and pg_net extensions
 CREATE EXTENSION IF NOT EXISTS pg_cron;
+CREATE EXTENSION IF NOT EXISTS pg_net;
 
 -- Trackers table
 CREATE TABLE trackers (
